1dab65cc2f881d933a3dad7dfeb594d624439596
Srinidhi M D <srinidhidevraj2267@gmail.com>
2026-07-27 13:40:32 +0530

    update the url

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@
    VisionCraft AI - Backend API
 ========================================== */
 
-const BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "https://visioncraft-ai-backend-5g6f.onrender.com";
 
 async function healthCheck() {
 
